8f6dda15138a68ed9fcb1a29d15fb778ee661cf5
monica.tobon <monica.tobon@fira-live.com>
2023-02-16 07:31:39 -0500

    ejemplo flatmap

--- src/Main2.java
+++ src/Main2.java
@@ -1,10 +1,25 @@
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Main2 {
 
-    public static void main(String args[]){
+    public static void main(String[] args){
 
         Generico generico =  new Generico();
-        Long numero = (Long) generico.verNumero(987L,798L);
+        Long numero = generico.verNumero(987L,798L);
         System.out.println("El valor de retorno es: "+numero);
+
+
+        List<String> stringList = List.of("f","g","c","d","f","d","a","a");
+        List<String> stringList2 = List.of("i","j","k","d","f","d","a","a");
+        List<String> stringList3 = Stream.of(stringList, stringList2)
+                .flatMap(Collection::stream)
+                .collect(Collectors.toList());
+        System.out.println(stringList3);
+
+        System.out.println(stringList.stream().distinct().collect(Collectors.toList()));
 
     }
 }
